761b01c7ff4e14f8eb657202d189342b3eb1e90f
GeOdin <madeleine.vanwinkel@gmail.com>
2016-02-18 15:18:47 +0100

    Added collision detection, both div1 and div2 change upon collision
    index.html
    * modified small comment to include date last modified and jQuery
    reference
    * added class "headerText" to header #div and #footerText
    * removed h3 tag in header and footer
    
    stylesheet.css
    * modified comment to include date last modified
    * added comment to include which HTML-element have been included in this
    CSS-file
    * added styling for header
    * added styling for class headerText
    * changed position of img from relative to absolute
    * added more styling for #flame
    * added #firehose
    * modified styling for #lighter
    * added styling for #wood
    * modified height of footer
    * added more styling for footer
    
    script.js
    * modified comment to include date last modified
    * included comment to show which functions have been implemented
    * added function main() to regulate the game
    * added function collision (div1, div2) to check whether there is a
    collision between 2 div2
    * added function collisionDetected (div1, div2) to regulate changing the
    image of div1 ad div2 and the location of div2 when a collision is
    detected

--- script.js
+++ script.js
@@ -3,52 +3,188 @@
  * JavaScript file for a Random Fire Game
  * Made on 2016-02-17
  * by GeOdin
+ * Last modified on 2016-02-18
  */
 
-$(document).ready(function() {
+/*
+ * This JavaScript-file contains the following functions:
+ * main
+ * collision (div1, div2)
+ * collisionDetected (div1, div2)
+ */
+
+// Function to regulate the game
+var main = function() {
+	// Create variables
+	var divs = new Object();
+	var div1 = $("#flame");
+	var div2 = $("#firehose");
+	divs.div1 = div1;
+	divs.div2 = div2;
+
+	// Function to arrange up, right, down, left input
     $(document).keydown(function(key) {
         switch(parseInt(key.which,10)) {
 			// Left arrow key pressed
 			case 37:
-				$("#flame").animate({left: "-=20px"}, "fast");
+				div1.animate({left: "-=20px"}, "fast");
+				divs = collision(div1, div2);
+				div1 = divs.div1;
+				div2 = divs.div2;
 				break;
 			// Up Arrow Pressed
 			case 38:
-				$("#flame").animate({top: "-=20px"}, "fast");
+				div1.animate({top: "-=20px"}, "fast");
+				divs = collision(div1, div2);
+				div1 = divs.div1;
+				div2 = divs.div2;
 				break;
 			// Right Arrow Pressed
 			case 39:
-				$("#flame").animate({left: "+=20px"}, "fast");
+				div1.animate({left: "+=20px"}, "fast");
+				divs = collision(div1, div2);
+				div1 = divs.div1;
+				div2 = divs.div2;
 				break;
 			// Down Arrow Pressed
 			case 40:
-				$("#flame").animate({top: "+=20px"}, "fast");
+				div1.animate({top: "+=20px"}, "fast");
+				divs = collision(div1, div2);
+				div1 = divs.div1;
+				div2 = divs.div2;
 				break;
 		}
 	});
-});
-
-// Collision function?
-//// http://stackoverflow.com/questions/5419134/how-to-detect-if-two-divs-touch-with-jquery
-/*
-function collision($div1, $div2) {
-	var x1 = $div1.offset().left;
-	var y1 = $div1.offset().top;
-	var h1 = $div1.outerHeight(true);
-	var w1 = $div1.outerWidth(true);
-	var b1 = y1 + h1;
-	var r1 = x1 + w1;
-	var x2 = $div2.offset().left;
-	var y2 = $div2.offset().top;
-	var h2 = $div2.outerHeight(true);
-	var w2 = $div2.outerWidth(true);
-	var b2 = y2 + h2;
-	var r2 = x2 + w2;
-
-	if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-		return false;
-	} else {
-		return true;
-	}
 }
- */
+
+// Function to check whether 2 divs collide
+var collision = function(div1, div2) {
+
+	// Get the left, top, right and bottom coordinates of div1, relative to the document
+	var div1left = div1.offset().left;
+	var div1top = div1.offset().top;
+	var div1height = div1.outerHeight();
+	var div1width = div1.outerWidth();
+	var div1bottom = div1top + div1height;
+	var div1right = div1left + div1width;
+
+	// Get the left, top, right and bottom coordinates of div2, relative to the document
+	var div2left = div2.offset().left;
+	var div2top = div2.offset().top;
+	var div2height = div2.outerHeight();
+	var div2width = div2.outerWidth();
+	var div2bottom = div2top + div2height;
+	var div2right = div2left + div2width;
+
+	// Check wheter the divs collide
+	if (div1left >= div2left) {
+		if (div1left < div2right) {
+			if (div1top >= div2top) {
+				if (div1top < div2bottom) {
+					divs = collisionDetected(div1, div2);
+					div1 = divs.div1;
+					div2 = divs.div2;
+				}
+			} else if (div1bottom <= div2bottom) {
+				if (div1bottom > div2top) {
+					divs = collisionDetected(div1, div2);
+					div1 = divs.div1;
+					div2 = divs.div2;
+				}
+			}
+		}
+	}
+	if (div1right <= div2right) {
+		if (div1right > div2left) {
+			if (div1bottom <= div2bottom) {
+				if (div1bottom > div2top) {
+					divs = collisionDetected(div1, div2);
+					div1 = divs.div1;
+					div2 = divs.div2;
+				}
+			} else if (div1top >= div2top) {
+				if (div1top < div2bottom) {
+					divs = collisionDetected(div1, div2);
+					div1 = divs.div1;
+					div2 = divs.div2;
+				}
+			}
+		}
+	}
+
+	// Return div1 and div2
+	var divs = new Object();
+	divs.div1 = div1;
+	divs.div2 = div2;
+	return divs;
+}
+
+// Function to deal with collision
+var collisionDetected = function(div1, div2) {
+	// Set variables
+	var div2temp;
+	var div1temp;
+	var randomTop;
+	var randomLeft;
+	var div1top;
+	var div1left;
+
+	// Save the div2
+	if ($("#firehose").is(":visible")) {
+		div2temp = "#firehose";
+	} else {
+		div2temp = "#wood";
+	}
+	// Hide div2
+	div2.hide(); // also sets picture back to upperleft corner
+	// Change the image of the div2
+	if (div2temp === "#firehose") {
+		div2 = $("#wood");
+	} else if (div2temp === "#wood") {
+		div2 = $("#firehose");
+	}
+	// Give it random coordinates within the webpage borders
+	randomTop = window.innerHeight * Math.random();
+	while (randomTop < 0 || randomTop > (window.outerHeight - 65)) {
+		randomTop = window.innerHeight * Math.random();
+	}
+	randomLeft = window.innerWidth * Math.random();
+	while (randomLeft < 0 || randomLeft > (window.outerWidth - 75)) {
+		randomLeft = window.innerWidth * Math.random();
+	}
+	div2.css({top: randomTop, left: randomLeft, position: "absolute"});
+	// Show the div2
+	div2.show();
+
+	// Save the div1
+	if ($("#flame").is(":visible")) {
+		div1temp = "#flame";
+	} else {
+		div1temp = "#lighter";
+	}
+	// Get the left, top, right and bottom coordinates of div1, relative to the document
+	div1left = div1.offset().left;
+	div1top = div1.offset().top;
+	// Hide div1
+	div1.hide(); // also sets picture back to upperleft corner
+	// Change the image of the div2
+	if (div1temp === "#flame") {
+		div1 = $("#lighter");
+		document.getElementById("footerText").innerHTML = "Relight My Fire";
+	} else if (div1temp === "#lighter") {
+		div1 = $("#flame");
+		document.getElementById("footerText").innerHTML = "Lopend Vuurtje";
+	}
+	// Give it the coordinates of the former div1
+	div1.css({top: div1top, left: div1left, position: "absolute"});
+	// Show the div1
+	div1.show();
+
+	// Return div1 and div2
+	var divs = new Object();
+	divs.div1 = div1;
+	divs.div2 = div2;
+	return divs;
+}
+
+$(document).ready(main);
